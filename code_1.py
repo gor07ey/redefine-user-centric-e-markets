567c19b14dd8036eefb3ca14c437bfe91d20efcb
gor07ey <152075976+gor07ey@users.noreply.github.com>
2023-12-16 10:54:48 +0300

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,29 @@
+function generateRandomData() {
+    const data = [];
+function main() {
+main();
+
+        console.log("Random Number: " + item);
+    data.forEach(item => {
+}
+    return data;
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+
+    const data = generateRandomData();
+
+    for (let i = 0; i < 10; i++) {
+}
+    }    public static void main(String[] args) {
+        }
+
+public class RandomDataGenerator {
+}
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
